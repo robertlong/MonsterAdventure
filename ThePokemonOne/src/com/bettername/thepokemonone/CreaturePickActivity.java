c2044a41133897752a0b7dd4e82e06cbbd364829
Kyle Lucas <kglucas93@gmail.com>
2013-01-26 15:27:21 -0800

    Added Creature Picker Activity

---
+++ ThePokemonOne/src/com/bettername/thepokemonone/CreaturePickActivity.java
@@ -0,0 +1,11 @@
+package com.bettername.thepokemonone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreaturePickActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creaturepick);
+    }
+}
