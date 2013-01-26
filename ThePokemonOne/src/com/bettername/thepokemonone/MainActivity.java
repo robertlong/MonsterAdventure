17f3ff1600cb29d2547e8aad95f139935b2bf47b
Nolan <nolan@nolan-laptop>
2013-01-26 11:55:59 -0800

    base code for activity

--- ThePokemonOne/src/com/bettername/thepokemonone/MainActivity.java
+++ ThePokemonOne/src/com/bettername/thepokemonone/MainActivity.java
@@ -2,17 +2,34 @@ package com.bettername.thepokemonone;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
-    
+    Context appContext = this;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button boyButton = (Button)findViewById(R.id.boy_button);
+        boyButton.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v)
+            {
+                
+                
+            }
+            
+        });
     }
     
     @Override
@@ -22,5 +39,32 @@ public class MainActivity extends Activity
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Toast.makeText(this, "you clicked an action bar button. Good for you.", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    @Override
+    protected void onPause()
+    {
+        // TODO Auto-generated method stub
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        // TODO Auto-generated method stub
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        // TODO Auto-generated method stub
+        super.onStop();
+    }
 }
