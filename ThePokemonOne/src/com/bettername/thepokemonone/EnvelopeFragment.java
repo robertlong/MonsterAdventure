baacedfb1a89f2541b40b3650e568fe9649b59c0
Nolan <nolan@nolan-laptop>
2013-01-27 13:29:29 -0800

    envelope

--- ThePokemonOne/src/com/bettername/thepokemonone/EnvelopeFragment.java
+++ ThePokemonOne/src/com/bettername/thepokemonone/EnvelopeFragment.java
@@ -1,14 +1,16 @@
 package com.bettername.thepokemonone;
 
 import android.app.DialogFragment;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 public class EnvelopeFragment extends DialogFragment
 {
-    static EnvelopeFragment newInstance(int num)
+    static EnvelopeFragment newInstance()
     {
         EnvelopeFragment f = new EnvelopeFragment();
         return f;
@@ -20,8 +22,10 @@ public class EnvelopeFragment extends DialogFragment
     {
         super.onCreate(savedInstanceState);
         View v = inflater.inflate(R.layout.envelope_fragment, container, false);
-        setStyle(DialogFragment.STYLE_NO_FRAME, android.R.style.Theme_Holo);
+        setStyle(DialogFragment.STYLE_NO_FRAME,
+                android.R.style.Theme_DeviceDefault_Dialog_NoActionBar_MinWidth);
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
         return v;
     }
-    
 }
