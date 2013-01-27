51fa01a2e0175026f1d0c8e8a501f59706fe68f3
Kristian Lyngbaek <kristianlyngbaek@gmail.com>
2013-01-27 10:50:36 -0800

    story activity

--- ThePokemonOne/src/com/bettername/thepokemonone/activity/StoryActivity.java
+++ ThePokemonOne/src/com/bettername/thepokemonone/activity/StoryActivity.java
@@ -1,5 +1,46 @@
 package com.bettername.thepokemonone.activity;
 
-public class StoryActivity {
+import com.bettername.thepokemonone.R;
+import com.bettername.thepokemonone.R.layout;
+import com.bettername.thepokemonone.R.menu;
+import com.bettername.thepokemonone.model.Player;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class StoryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_story);
+
+//		final Button continueButton = (Button) findViewById(R.id.continue_button);
+//		continueButton.setOnClickListener(new OnClickListener()
+//        {
+//            
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent continueIntent = new Intent(appContext, StoryActivty.class);
+//                appContext.startActivity(continueIntent);
+//            }
+//        });
+		
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_story, menu);
+		return true;
+	}
 
 }
