da9ede8da5e10f0d0d7350f89f9ed9bdc2f0a5b3
Nolan <nolan@nolan-laptop>
2013-01-27 15:03:55 -0800

    FINISHDIHDIFSJIEL

--- ThePokemonOne/src/com/bettername/thepokemonone/StoryActivity.java
+++ ThePokemonOne/src/com/bettername/thepokemonone/StoryActivity.java
@@ -6,39 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class StoryActivity extends Activity {
-
+public class StoryActivity extends Activity
+{
+    
     Context appContext = this;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_story);
-
-		final Button continueButton = (Button) findViewById(R.id.button1);
-		continueButton.setOnClickListener(new OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent continueIntent = new Intent(appContext, MapActivity.class);
-                appContext.startActivity(continueIntent);
-            }
-        });
-		
-		
-		
-		
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_story, menu);
-		return true;
-	}
-
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+        
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_story, menu);
+        return true;
+    }
+    
+    public void onClick(View v)
+    {
+        
+        Intent continueIntent = new Intent(appContext, MapActivity.class);
+        appContext.startActivity(continueIntent);
+        
+    }
+    
 }
