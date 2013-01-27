076149c302593c8b8bbfdbf44d67829c37975be6
Kristian Lyngbaek <kristianlyngbaek@gmail.com>
2013-01-26 19:38:12 -0800

    Added new pedestal pics

---
+++ ThePokemonOne/src/com/bettername/thepokemonone/Config.java
@@ -0,0 +1,17 @@
+package com.bettername.thepokemonone;
+import android.content.Context;
+import android.provider.Settings.Secure;
+
+
+public class Config {
+
+	private static Context context;
+	public static void setContext(Context context) {
+		Config.context = context;
+	}
+	
+	public static String getUdid() {
+		String android_id = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
+		return android_id;
+    }
+}
